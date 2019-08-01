6ea1d5abf0641a584ffcffc78fb7f50234ff75bd
Darío Milanés <dario.milanes@gmail.com>
2019-08-01 20:49:18 +0100

    Modified UserProfile the page

--- src/components/users/FeedUsers.jsx
+++ src/components/users/FeedUsers.jsx
@@ -1,108 +1,76 @@
 import React from 'react'
-import { Feed, Icon } from 'semantic-ui-react'
+import { Button, Comment, Form, Header } from 'semantic-ui-react'
 
-const FeedUser = () => (
-    <Feed>
-        <Feed.Event>
-            <Feed.Label>
-                <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
-            </Feed.Label>
-            <Feed.Content>
-                <Feed.Summary>
-                    <Feed.User>Elliot Fu</Feed.User> added you as a friend
-          <Feed.Date>1 Hour Ago</Feed.Date>
-                </Feed.Summary>
-                <Feed.Meta>
-                    <Feed.Like>
-                        <Icon name='like' />
-                        4 Likes
-          </Feed.Like>
-                </Feed.Meta>
-            </Feed.Content>
-        </Feed.Event>
+const FeedUsers = () => (
+    <Comment.Group>
+        <Header as='h3' dividing>
+            Comments
+    </Header>
 
-        <Feed.Event>
-            <Feed.Label image='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
-            <Feed.Content>
-                <Feed.Summary>
-                    <a>Helen Troy</a> added <a>2 new illustrations</a>
-                    <Feed.Date>4 days ago</Feed.Date>
-                </Feed.Summary>
-                <Feed.Extra images>
-                    <a>
-                        <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </a>
-                    <a>
-                        <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </a>
-                </Feed.Extra>
-                <Feed.Meta>
-                    <Feed.Like>
-                        <Icon name='like' />
-                        1 Like
-          </Feed.Like>
-                </Feed.Meta>
-            </Feed.Content>
-        </Feed.Event>
+        <Comment>
+            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
+            <Comment.Content>
+                <Comment.Author as='a'>Matt</Comment.Author>
+                <Comment.Metadata>
+                    <div>Today at 5:42PM</div>
+                </Comment.Metadata>
+                <Comment.Text>How artistic!</Comment.Text>
+                <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+            </Comment.Content>
+        </Comment>
 
-        <Feed.Event>
-            <Feed.Label image='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
-            <Feed.Content>
-                <Feed.Summary date='2 Days Ago' user='Jenny Hess' content='add you as a friend' />
-                <Feed.Meta>
-                    <Feed.Like>
-                        <Icon name='like' />
-                        8 Likes
-          </Feed.Like>
-                </Feed.Meta>
-            </Feed.Content>
-        </Feed.Event>
+        <Comment>
+            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+            <Comment.Content>
+                <Comment.Author as='a'>Elliot Fu</Comment.Author>
+                <Comment.Metadata>
+                    <div>Yesterday at 12:30AM</div>
+                </Comment.Metadata>
+                <Comment.Text>
+                    <p>This has been very useful for my research. Thanks as well!</p>
+                </Comment.Text>
+                <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+            </Comment.Content>
+            <Comment.Group>
+                <Comment>
+                    <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
+                    <Comment.Content>
+                        <Comment.Author as='a'>Jenny Hess</Comment.Author>
+                        <Comment.Metadata>
+                            <div>Just now</div>
+                        </Comment.Metadata>
+                        <Comment.Text>Elliot you are always so right :)</Comment.Text>
+                        <Comment.Actions>
+                            <Comment.Action>Reply</Comment.Action>
+                        </Comment.Actions>
+                    </Comment.Content>
+                </Comment>
+            </Comment.Group>
+        </Comment>
 
-        <Feed.Event>
-            <Feed.Label image='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
-            <Feed.Content>
-                <Feed.Summary>
-                    <a>Joe Henderson</a> posted on his page
-          <Feed.Date>3 days ago</Feed.Date>
-                </Feed.Summary>
-                <Feed.Extra text>
-                    Ours is a life of constant reruns. We're always circling back to where we'd we started,
-                    then starting all over again. Even if we don't run extra laps that day, we surely will
-                    come back for more of the same another day soon.
-        </Feed.Extra>
-                <Feed.Meta>
-                    <Feed.Like>
-                        <Icon name='like' />
-                        5 Likes
-          </Feed.Like>
-                </Feed.Meta>
-            </Feed.Content>
-        </Feed.Event>
+        <Comment>
+            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+            <Comment.Content>
+                <Comment.Author as='a'>Joe Henderson</Comment.Author>
+                <Comment.Metadata>
+                    <div>5 days ago</div>
+                </Comment.Metadata>
+                <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
+                <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+            </Comment.Content>
+        </Comment>
 
-        <Feed.Event>
-            <Feed.Label image='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
-            <Feed.Content>
-                <Feed.Summary>
-                    <a>Justen Kitsune</a> added <a>2 new photos</a> of you
-          <Feed.Date>4 days ago</Feed.Date>
-                </Feed.Summary>
-                <Feed.Extra images>
-                    <a>
-                        <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </a>
-                    <a>
-                        <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </a>
-                </Feed.Extra>
-                <Feed.Meta>
-                    <Feed.Like>
-                        <Icon name='like' />
-                        41 Likes
-          </Feed.Like>
-                </Feed.Meta>
-            </Feed.Content>
-        </Feed.Event>
-    </Feed>
+        <Form reply>
+            <Form.TextArea />
+            <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+        </Form>
+    </Comment.Group>
 )
 
-export default FeedUser
+export default FeedUsers
--- src/components/users/PetMiniCard.jsx
+++ src/components/users/PetMiniCard.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
-import { Card, Image } from 'semantic-ui-react'
+import { Card, Image, Icon } from 'semantic-ui-react'
 
 
 const PetMiniCard = () => (
-    <Card.Group>
+    <Card.Group centered>
         <Card>
             <Card.Content>
                 <Image floated='right' circular size='mini' src='https://saudeplanoparapet.nsbeneficios.com.br/img/carenciazero_pet_mobile.png' />
-                <Card.Header>Firulai</Card.Header>
+                <Card.Header><Icon name="paw" />Firulai</Card.Header>
                 <Card.Meta>Friends of Elliot</Card.Meta>
                 <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
+                    A Firulai le encanta las pelotas.
                 </Card.Description>
             </Card.Content>
         </Card>
         <Card>
             <Card.Content>
                 <Image floated='right' circular size='mini' src='https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg' />
-                <Card.Header>Toby</Card.Header>
+
+                <Card.Header><Icon name="paw" />Toby</Card.Header>
                 <Card.Meta>New User</Card.Meta>
                 <Card.Description>
-                    Molly wants to add you to the group <strong>musicians</strong>
+                    Todo el tiempo Toby caza lagartos en el parque.
                 </Card.Description>
             </Card.Content>
         </Card>
