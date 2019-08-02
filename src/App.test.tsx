2708a4ea830ce46b8c8bd21e53ec1234c29edd03
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-08-02 20:36:45 +0100

    Added testing libraries and change the App component test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, cleanup } from "@testing-library/react"; 
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
+afterEach(cleanup)
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App />);
 });
