ac8e4bcc5babc01ad96bbc556da0153adf14bb0e
Aarón Pérez <aaron@pop-os.localdomain>
2019-08-04 00:35:24 +0100

    NearUsers view now displays in a responsive and ordenate way all the mocked users, doesnt care which is the quantity.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import HomePage from './pages/Home'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/Home';
 
-import HeaderFanimals from './components/shared/HeaderFanimals'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import UserProfile from './pages/UserProfile'
-import About from './pages/About'
+import HeaderFanimals from './components/shared/HeaderFanimals';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import UserProfile from './pages/UserProfile';
+import About from './pages/About';
+import NearUsers from './pages/NearUsers';
 
-import birdsbackground from './assets/matthew.png';
+import birdsbackground from './assets/birdsbackground.png';
 
 const styles = {
   // linear-gradient(120deg, rgba(255, 178, 243, 0.6), transparent)
@@ -34,6 +35,7 @@ const App = () => {
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/user" component={UserProfile} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/nearusers" component={NearUsers} />
 
 
 
