9dd7a580120f5d1c444212158a860e225ad4f8c4
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-08-05 03:14:29 +0100

    Some tests

--- _templates/hygen/index.js
+++ _templates/hygen/index.js
@@ -2,14 +2,17 @@ const { runner } = require('hygen')
 const Logger = require('hygen/lib/logger')
 const path = require('path')
 const defaultTemplates = path.join(__dirname, 'templates')
+const searcher = require('./searcher')
 
+console.log(searcher)
 runner(process.argv.slice(2), {
   templates: defaultTemplates,
   cwd: process.cwd(),
   logger: new Logger(console.log.bind(console)),
   createPrompter: () => require('enquirer'),
-  exec: (action, body) => {
-    const opts = body && body.length > 0 ? { input: body } : {}
+  exec: (action, searcher) => {
+    //The input body is the hygen execute
+    const opts = searcher && searcher.length > 0 ? { input: searcher } : {}
     return require('execa').shell(action, opts)
   },
   debug: !!process.env.DEBUG
