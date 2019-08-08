53c5d267b76da09f2bb404f03a9bf439beebda7f
Aarón Pérez <aarperper@gmail.com>
2019-08-08 22:14:43 +0100

    Refactor

--- src/components/maps/MiniMap.jsx
+++ src/components/maps/MiniMap.jsx
@@ -1,8 +1,8 @@
-import React, { Component, useState, useEffect } from "react";
+import React from "react";
 import GoogleMapReact from "google-map-react";
 export const MiniMap = props => {
   return (
-    <div style={{ height: "250px", width: "250px", marginLeft: "" }}>
+    <div style={{ height: "250px", width: "250px" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyBzGVNtpx96mevl5hXFpx7n-ZeAeM3u1k8" }}
         center={{
