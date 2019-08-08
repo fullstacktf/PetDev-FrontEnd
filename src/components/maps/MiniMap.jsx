b84913bb573696b14807599293ee07e6d3c30b84
Aarón Pérez <aarperper@gmail.com>
2019-08-08 23:21:50 +0100

    Removed API key from code

--- src/components/maps/MiniMap.jsx
+++ src/components/maps/MiniMap.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
+import apiKey from "../../apiKey.json"
 export const MiniMap = props => {
   return (
     <div style={{ height: "250px", width: "250px" }}>
+        {console.log(apiKey)}
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyBzGVNtpx96mevl5hXFpx7n-ZeAeM3u1k8" }}
+        bootstrapURLKeys=/* { apiKey }   */"AIzaSyBzGVNtpx96mevl5hXFpx7n-ZeAeM3u1k8"
         center={{
           lat: props.lat,
           lng: props.lng
