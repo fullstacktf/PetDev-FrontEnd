03c248b3a59ee38098c0fa27a6bd2109c2c02016
Aarón Pérez <aarperper@gmail.com>
2019-08-13 03:07:43 +0100

    Gallery updated, autoplay disabled and now you must drag to see the other pics

--- src/components/users/GridImages.jsx
+++ src/components/users/GridImages.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import "react-alice-carousel/lib/alice-carousel.css";
+import AliceCarousel from 'react-alice-carousel';
+import styled from "@emotion/styled";
 
-const GridImages = () => (
-  <Grid>
-    <Grid.Row columns={3}>
-      <Grid.Column>
-        <Image bordered rounded size='large' src='https://source.unsplash.com/random/400x200' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image bordered rounded size='large' src='https://source.unsplash.com/random/500x281.25' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image bordered rounded size='large' src='https://source.unsplash.com/random/600x300' />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-)
+const Image = styled.img`
+border: 1px solid grey;
+box-shadow: 0.5px 0.5px 5px black;
+border-radius: 5px;
+` 
+
+const GridImages = () => {
+  const handleOnDragStart = e => e.preventDefault()
+  return (
+    <AliceCarousel  buttonsDisabled  mouseDragEnabled >
+      <Image src="https://source.unsplash.com/random/500x301"  onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <Image src="https://source.unsplash.com/random/500x302"   onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <Image src="https://source.unsplash.com/random/500x303"   onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <Image src="https://source.unsplash.com/random/500x304"   onDragStart={handleOnDragStart} className="yours-custom-class" />
+      <Image src="https://source.unsplash.com/random/500x300"   onDragStart={handleOnDragStart} className="yours-custom-class" />
+    </AliceCarousel>
+  )
+}
+
 
 export default GridImages
